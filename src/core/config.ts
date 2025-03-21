e4dbebfb8c0c90cf591bce146f3e4bc099bfdf21
Onat Aktaş <onat_akts@hotmail.com>
2025-03-21 18:00:59 +0300

    fix: Update asset paths and adjust target shape number for consistency

--- src/core/config.ts
+++ src/core/config.ts
@@ -6,23 +6,23 @@ export const SAFESIZE: GameSize = { width: 800, height: 600 };
 
 
 export const SPRITES: spriteConfig[] = [
-    { alias: "mainscenebackground", src: "/assets/sprites/background.jpg" },
-    { alias: "ship", src: "/assets/sprites/shapes/ship.png" },
-    { alias: "bottle", src: "/assets/sprites/shapes/bottle.png" },
-    { alias: "high_heels", src: "/assets/sprites/shapes/high_heels.png" },
-    { alias: "table_lamp", src: "/assets/sprites/shapes/table_lamp.png" },
-    { alias: "frame", src: "/assets/sprites/frame.png" },
-    { alias: "buttonbg", src: "/assets/sprites/fail_bg.png" },
-    { alias: "progressbarbg", src: "/assets/sprites/progressbar_bg.png" },
-    { alias: "progressbarfill", src: "/assets/sprites/progressbar_fill.png" },
-    { alias: "icon", src: "/assets/sprites/brain_icon.png" },
-    { alias: "autoplayhand", src: "/assets/sprites/hand.png" },
-    { alias: "rect", src: "/assets/sprites/rect.png" },
-    { alias: "failimage", src: "/assets/sprites/fail_bg.png" },
-    { alias: "welldoneimage", src: "/assets/sprites/well_done_bg.png" },
-    { alias: "downloadbutton", src: "/assets/sprites/download_button.png" },
+    { alias: "mainscenebackground", src: "./assets/sprites/background.jpg" },
+    { alias: "ship", src: "./assets/sprites/shapes/ship.png" },
+    { alias: "bottle", src: "./assets/sprites/shapes/bottle.png" },
+    { alias: "high_heels", src: "./assets/sprites/shapes/high_heels.png" },
+    { alias: "table_lamp", src: "./assets/sprites/shapes/table_lamp.png" },
+    { alias: "frame", src: "./assets/sprites/frame.png" },
+    { alias: "buttonbg", src: "./assets/sprites/fail_bg.png" },
+    { alias: "progressbarbg", src: "./assets/sprites/progressbar_bg.png" },
+    { alias: "progressbarfill", src: "./assets/sprites/progressbar_fill.png" },
+    { alias: "icon", src: "./assets/sprites/brain_icon.png" },
+    { alias: "autoplayhand", src: "./assets/sprites/hand.png" },
+    { alias: "rect", src: "./assets/sprites/rect.png" },
+    { alias: "failimage", src: "./assets/sprites/fail_bg.png" },
+    { alias: "welldoneimage", src: "./assets/sprites/well_done_bg.png" },
+    { alias: "downloadbutton", src: "./assets/sprites/download_button.png" },
 ]
 
 export const FONTS: FontFace[] = [
-    new FontFace('sniglet-regular', 'url(assets/fonts/Sniglet-Regular.ttf)')
+    new FontFace('sniglet-regular', 'url(./assets/fonts/Sniglet-Regular.ttf)')
 ];
--- src/core/sound/config.ts
+++ src/core/sound/config.ts
@@ -3,22 +3,22 @@ import { SoundChannel, SoundConfig } from "./types";
 export const SOUNDS : SoundConfig[] = [
     {
         alias: "win",
-        url: "assets/sounds/effect_win.mp3",
+        url: "./assets/sounds/effect_win.mp3",
         soundChannel: SoundChannel.EFFECT,
     },
     {
         alias: "lose",
-        url: "assets/sounds/effect_lose.mp3",
+        url: "./assets/sounds/effect_lose.mp3",
         soundChannel: SoundChannel.EFFECT,
     },
     {
         alias: "confetti",
-        url: "assets/sounds/effect_confetti.mp3",
+        url: "./assets/sounds/effect_confetti.mp3",
         soundChannel: SoundChannel.EFFECT,
     },
     {
         alias: "theme",
-        url: "assets/sounds/theme_main.mp3",
+        url: "./assets/sounds/theme_main.mp3",
         soundChannel: SoundChannel.MUSIC,
     }
 ]
